633110f51fd68c4d44cb0749dad99b76ae800800
yahya-mohammed07 <goldroger300@gmail.com>
2024-02-20 00:24:45 +0300

    responsive navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,7 +1,76 @@
 import React, { ReactNode } from "react";
 import styles from "./navbar.module.css";
+import { GiHamburgerMenu } from "react-icons/gi";
+import useIsNavOpenState from "../../states/openState";
+
+interface CustomLinkType {
+  href: string;
+  children: ReactNode;
+}
+
+const CustomLinkMobile: React.FC<CustomLinkType> = ({ href, children }) => {
+  return (
+    <li className={`${styles.list} p-6 text-3xl`}>
+      <a className={styles.linkA} href={href}>
+        {children}
+      </a>
+    </li>
+  );
+};
+
+// MobileNavbar component for smaller screens
+const MobileNavbar = () => {
+  const { isNavOpen, setNavOpen } = useIsNavOpenState();
+
+  const toggleMobileNav = () => {
+    setNavOpen(!isNavOpen);
+  };
+  // box to hold col of urls hidden in large screens
+  // top-full class ensures it starts below the top edge of its containing block.
+  // right-0 class places it against the right edge
+  // z-10 class ensures it appears above other elements with a lower z-index.
+  return (
+    <>
+      {open && (
+        <div
+          className={
+            !open
+              ? "hidden"
+              : "sm:hidden flex justify-center items-center m-auto max-w-96 mx-auto absolute top-full right-0 left-0 bg-white rounded-lg overflow-hidden shadow-xl border z-50"
+          }
+        >
+          <ul
+            className="flex flex-col text-sm h-full w-svw p-4 text-center list-none relative overflow-auto"
+            onClick={toggleMobileNav}
+          >
+            <CustomLinkMobile href="#section_about">About</CustomLinkMobile>
+            <CustomLinkMobile href="#section_skills">Skills</CustomLinkMobile>
+            <CustomLinkMobile href="#section_project">
+              Projects
+            </CustomLinkMobile>
+            <CustomLinkMobile href="#section_contact">
+              Let's Collaborate ;)
+            </CustomLinkMobile>
+          </ul>
+        </div>
+      )}
+    </>
+  );
+};
+
+const CustomLink: React.FC<CustomLinkType> = ({ href, children }) => {
+  return (
+    <li className={styles.list}>
+      <a className={styles.linkA} href={href}>
+        {children}
+      </a>
+    </li>
+  );
+};
 
 const Navbar = () => {
+  const { isNavOpen, setNavOpen } = useIsNavOpenState();
+  const toggleNavbar = () => setNavOpen(!isNavOpen);
   return (
     <>
       <header>
@@ -10,10 +79,20 @@ const Navbar = () => {
         >
           <a
             href="#"
-            className={`${styles.linkA} -mt-3 text-3xl md:text-4xl font-bold`}
+            className={`${styles.linkA} -mt-2 text-3xl md:text-4xl font-bold`}
           >
             Yahya
           </a>
+          <button
+            //  hide in xl,lg
+            className="sm:hidden -mt-1 active:bg-slate-400 focus:outline-none hover:rounded-lg hover:bg-slate-300 p-2 text-xl"
+            onClick={toggleNavbar}
+          >
+            <GiHamburgerMenu />
+          </button>
+          {/*if click it true then show mobile navbar*/}
+          {isNavOpen && <MobileNavbar />}
+          {/*only show when screen sm size and hide show when clicked*/}
           <ul className={`sm:flex gap-1 list-none hidden`}>
             <CustomLink href="#section_about">About</CustomLink>
             <CustomLink href="#section_skills">Skills</CustomLink>
@@ -25,21 +104,6 @@ const Navbar = () => {
         </nav>
       </header>
     </>
-  );
-};
-
-interface CustomLinkType {
-  href: string;
-  children: ReactNode;
-}
-
-const CustomLink: React.FC<CustomLinkType> = ({ href, children }) => {
-  return (
-    <li className={styles.list}>
-      <a className={styles.linkA} href={href}>
-        {children}
-      </a>
-    </li>
   );
 };
 
